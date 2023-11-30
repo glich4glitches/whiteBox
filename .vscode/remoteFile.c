d9ff659fde1fcc6ef302864dde95390e77133afe
Gumsum Kaur <54205121+gumsum-kaur@users.noreply.github.com>
2023-11-30 17:24:32 +0000

    commit requested again

--- .vscode/remoteFile.c
+++ .vscode/remoteFile.c
@@ -1,6 +1,26 @@
+/**Incomplete Code...*/
+
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(){
+int sortMe(lst){
+    int n = sizeof(lst)/sizeof(lst[0]);
+
+    for(int i=0; i<=n-1;i++){
+        for(int j=0; j<=i;j++){
+            if(lst[j] >= lst[j+1]){
+                lst[j],lst[j+1] = lst[j+1], lst[j];
+            }
+        }
+    }
+
+    return lst;
+}
+
+int main()
+{
     printf("\nThis is the remote file written by the help of super-duper-obit dot dot dot..\n");
+    int lst[] = {5,6,7,8,9,9};
+    printf("Output: %d",sortMe(lst));
+    printf("\n");
 }
